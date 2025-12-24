bab24d1195dbb0b46335fba0df2c462bee81df0d
ronitsthaa <shrestharonit336@gmail.com>
2025-12-24 21:24:47 +0545

    Added search bar and error fix for not showing product name

--- frontend/fyp/src/pages/customerDashboard.jsx
+++ frontend/fyp/src/pages/customerDashboard.jsx
@@ -4,6 +4,7 @@ import "../styles/customerDashboard.css";
 
 const CustomerDashboard = () => {
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
@@ -25,27 +26,48 @@ const CustomerDashboard = () => {
     fetchProducts();
   }, []);
 
+  // Filter products based on search
+ const filteredProducts = products.filter((p) =>
+  (p.name || "").toLowerCase().includes(searchTerm.toLowerCase()) ||
+  (p.description || "").toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+
   if (loading) return <p className="loading">Loading products...</p>;
   if (error) return <p className="error">{error}</p>;
+  
 
   return (
     <div className="customer-container">
       <h1>Available Products</h1>
 
-      <div className="product-grid">
-        {products.map((p) => (
-          <div className="product-card" key={p._id}>
-            <img
-              src={`${API_BASE}/${p.image}`}
-              alt={p.name}
-            />
-            <h3>{p.name}</h3>
-            <p className="price">NPR {p.price}</p>
-            <p className="desc">{p.description}</p>
+      {/* Search Bar */}
+      <input
+        type="text"
+        placeholder="Search products..."
+        className="search-bar"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
 
-            <button className="buy-btn">Buy Now</button>
-          </div>
-        ))}
+      <div className="product-grid">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((p) => (
+            <div className="product-card" key={p._id}>
+              <img
+                src={`${API_BASE}/${p.image}`}
+                alt={p.name}
+              />
+              <h3>{p.name}</h3>
+              <p className="price">NPR {p.price}</p>
+              <p className="desc">{p.description}</p>
+
+              <button className="buy-btn">Buy Now</button>
+            </div>
+          ))
+        ) : (
+          <p className="no-results">No products found</p>
+        )}
       </div>
     </div>
   );
